e974357066408a43cf3af7b0c8aec9de42fb5b68
Nayan <kn4y4nnn@gmail.com>
2023-08-18 02:28:05 +0530

    feat: config handler

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,21 +1,33 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
+type config struct {
+	address   string
+	staticDir string
+}
+
 func main() {
+	var cfg config
+	flag.StringVar(&cfg.address, "addr", "localhost:4000", "HTTP network address")
+	flag.StringVar(&cfg.staticDir, "static", "./ui/static", "Static Directory for Assets")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	fileServer := http.FileServer(http.Dir(cfg.staticDir))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Print("http://localhost:4000 ...")
-	err := http.ListenAndServe("localhost:4000", mux)
+	log.Printf("Serving@ http://%s", cfg.address)
+
+	err := http.ListenAndServe(cfg.address, mux)
 	log.Fatal(err)
 }
