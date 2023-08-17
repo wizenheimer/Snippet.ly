852255a2ca0902cca4f616a32e2fb3015db74917
Nayan <kn4y4nnn@gmail.com>
2023-08-18 02:39:35 +0530

    feat:custom error logger

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 type config struct {
@@ -12,22 +13,28 @@ type config struct {
 }
 
 func main() {
+	// server configurations
 	var cfg config
 	flag.StringVar(&cfg.address, "addr", "localhost:4000", "HTTP network address")
 	flag.StringVar(&cfg.staticDir, "static", "./ui/static", "Static Directory for Assets")
 	flag.Parse()
 
+	// logger configuration
+	infoLogger := log.New(os.Stdout, "INFO:\t", log.Ldate|log.Ltime)
+	errorLogger := log.New(os.Stderr, "ERROR:\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// multiplexer and fileserver configuration
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir(cfg.staticDir))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
+	// routes
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Printf("Serving@ http://%s", cfg.address)
-
+	infoLogger.Printf("Serving@ http://%s", cfg.address)
 	err := http.ListenAndServe(cfg.address, mux)
-	log.Fatal(err)
+	errorLogger.Fatal(err)
 }
