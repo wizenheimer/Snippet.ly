afb2cf35998b24b113ac3ad0c6b21db1a6909e88
Nayan <kn4y4nnn@gmail.com>
2023-08-18 01:15:30 +0530

    feat: template handler

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,9 +7,14 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
+
 	log.Print("http://localhost:4000 ...")
 	err := http.ListenAndServe("localhost:4000", mux)
 	log.Fatal(err)
