7613950d03f335b3d8c04240a45204e6e36bec03
Nayan <wizenheimer@proton.me>
2023-08-27 07:57:26 +0530

    feat: add auth flow

--- internal/models/users.go
+++ internal/models/users.go
@@ -40,8 +40,28 @@ VALUES(?, ?, ?, UTC_TIMESTAMP())`
 	return nil
 }
 
+// Authenticate and return User ID
 func (m *UserModel) Authenticate(email, password string) (int, error) {
-	return 0, nil
+	var id int
+	var hashedPassword []byte
+	stmt := "SELECT id, hashed_password FROM users WHERE email = ?"
+	err := m.Db.QueryRow(stmt, email).Scan(&id, &hashedPassword)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return 0, ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
+	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return 0, ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
+	return id, nil
 }
 
 func (m *UserModel) Exists(email, password string) (bool, error) {
--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -7,13 +7,14 @@ import (
 )
 
 type Validator struct {
-	FieldErrors map[string]string
+	FieldErrors    map[string]string
+	NonFieldErrors []string
 }
 
 var EmailRegex = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
 func (v *Validator) Valid() bool {
-	return len(v.FieldErrors) == 0
+	return len(v.FieldErrors) == 0 && len(v.NonFieldErrors) == 0
 }
 
 func (v *Validator) AddFieldErrors(key string, message string) {
@@ -24,6 +25,10 @@ func (v *Validator) AddFieldErrors(key string, message string) {
 	if _, exists := v.FieldErrors[key]; !exists {
 		v.FieldErrors[key] = message
 	}
+}
+
+func (v *Validator) AddNonFieldErrors(msg string) {
+	v.NonFieldErrors = append(v.NonFieldErrors, msg)
 }
 
 func (v *Validator) CheckField(ok bool, key string, message string) {
